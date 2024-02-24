4e6d7be5647e190985e0ebe1f8be50b6f55e9630
CYRD <cyrdpromax@gmail.com>
2024-02-24 19:13:45 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/view')
+def view():
+    return render_template('view.html')
+
+@app.route('/review')
+def review():
+    return render_template('review.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+@app.route('/log')
+def log():
+    return render_template('log.html')
+
+@app.route('/detailed')
+def detailed():
+    return render_template('detailed.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
